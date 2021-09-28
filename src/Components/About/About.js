3274d76f6035f7b5b8ab161af255dad9285e5756
Patrick <patricklang87@gmail.com>
2021-09-28 14:40:31 -0400

    hide PDF

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -14,7 +14,9 @@ export default function About() {
                     <h2>About Me</h2>
                     <p>I am a self-taught web developer working to break into the industry. I'm constantly working to expand my skillset. I enjoy both both frontend and backend challenges, and, as a former language instructor, and interesting in learning more about NLP and manipulating language digitally!</p>
 
-                    <p><a href="https://github.com/patricklang87" target="_blank" rel='noreferrer'>GitHub</a> | <a href="https://www.linkedin.com/in/pslang/" target="_blank" rel='noreferrer' >LinkedIn</a> | <a href="/documents/Lang_Patrick_Resume_20210908.pdf" download>PDF Resume</a></p>
+                    <p><a href="https://github.com/patricklang87" target="_blank" rel='noreferrer'>GitHub</a> | <a href="https://www.linkedin.com/in/pslang/" target="_blank" rel='noreferrer' >LinkedIn</a>
+                    {/* | <a href="/documents/Lang_Patrick_Resume_20210908.pdf" download>PDF Resume</a> */}
+                    </p>
                     <p>Write to me: patricklang87 @ gmail.com</p>
                 </div>    
             </div>
