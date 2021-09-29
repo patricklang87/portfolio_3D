65f69cb3bb19975da51decc2166ab461a10348a8
Patrick <patricklang87@gmail.com>
2021-09-28 20:43:52 -0400

    correct typos

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -12,7 +12,7 @@ export default function About() {
                 </div>
                 <div className='aboutInfo'>
                     <h2>About Me</h2>
-                    <p>I am a self-taught web developer working to break into the industry. I'm constantly working to expand my skillset. I enjoy both both frontend and backend challenges, and, as a former language instructor, and interested in learning more about NLP and manipulating language digitally!</p>
+                    <p>I am a self-taught web developer working to break into the industry. I'm constantly working to expand my skillset. I enjoy both frontend and backend challenges, and, as a former language instructor, am interested in learning more about NLP and manipulating language digitally!</p>
 
                     <p><a href="https://github.com/patricklang87" target="_blank" rel='noreferrer'>GitHub</a> | <a href="https://www.linkedin.com/in/pslang/" target="_blank" rel='noreferrer' >LinkedIn</a>
                     {/* | <a href="/documents/Lang_Patrick_Resume_20210908.pdf" download>PDF Resume</a> */}
